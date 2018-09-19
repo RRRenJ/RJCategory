a3bed251bfb4a5466431d20c4e633e0932970385
RRRenj <rrrenj@outlook.com>
2018-09-19 16:30:04 +0800

    0.1.3 优化UIAlertController

--- RRRCategory/UI/UIAlertController+Simple.h
+++ RRRCategory/UI/UIAlertController+Simple.h
@@ -10,6 +10,16 @@
 
 @interface UIAlertController (Simple)
 
+/**
+ 简易创建alert
+
+ @param title title  为nil时就没有title
+ @param message message
+ @param style UIAlertControllerStyle
+ @param actions @[@{@"第一":[NSNumber numberWithInt:UIAlertActionStyleDefault]},@{@"第二":[NSNumber numberWithInt:UIAlertActionStyleDefault]},@{@"取消":[NSNumber numberWithInt:UIAlertActionStyleCancel]}]
+ @param handler integer 为选中选项
+ @return UIAlertController
+ */
 + (UIAlertController *)alertControllerWithTitle:(NSString *)title message:(NSString *)message preferredStyle:(UIAlertControllerStyle)style actions:(NSArray <NSDictionary <NSString *,NSNumber *>*>*)actions actionHandler:(void (^)(NSInteger integer))handler;
 
 @end
